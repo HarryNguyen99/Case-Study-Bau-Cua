783b7362441b25a60e64fb1a97e591f6ea997366
Harry Nguyen <50328363+HarryNguyen99@users.noreply.github.com>
2020-03-09 14:08:12 +0700

    random

---
+++ js/xiNgau.js
@@ -0,0 +1,38 @@
+class XiNgau {
+    constructor(changeImage1, changeImage2, changeImage3) {
+        this.changeImage1 = changeImage1;
+        this.changeImage2 = changeImage2;
+        this.changeImage3 = changeImage3;
+
+        this.getimage1 = function () {
+            return this.changeImage1 = Math.floor(Math.random() * 6);
+        }
+        this.getimage2 = function () {
+            return this.changeImage2 = Math.floor(Math.random() * 6);
+        }
+        this.getimage3 = function () {
+            return this.changeImage3 = Math.floor(Math.random() * 6);
+        }
+    }
+
+}
+
+function convert() {
+    let change_image1 = ["image/nai.jpg", "image/cua.jpg", "image/ga.jpg",
+        "image/ca.jpg", "image/bau.jpg", "image/tom.jpg"];
+    let change_image2 = ["image/nai.jpg", "image/cua.jpg", "image/ga.jpg",
+        "image/ca.jpg", "image/bau.jpg", "image/tom.jpg"];
+    let change_image3 = ["image/nai.jpg", "image/cua.jpg", "image/ga.jpg",
+        "image/ca.jpg", "image/bau.jpg", "image/tom.jpg"];
+
+    let xiNgau = new XiNgau(change_image1, change_image2, change_image3);
+    let change1 = xiNgau.getimage1();
+    setTimeout(change1, 10000);
+    document.getElementById("xiNgau1").src = change_image1 [change1];
+    let change2 = xiNgau.getimage2();
+    document.getElementById("xiNgau2").src = change_image2 [change2];
+    let change3 = xiNgau.getimage3();
+    document.getElementById("xiNgau3").src = change_image3 [change3];
+}
+
+
